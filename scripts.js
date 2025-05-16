0d7dc037969d9c464fe74e6b37ad12011adbb5ee
BefunkyEtsy <befunkyetsy@gmail.com>
2025-05-16 12:40:03 +0100

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,23 +1,17 @@
-// Load the list of books from books.json
 fetch('books.json')
     .then(response => response.json())
     .then(data => {
         const bookList = document.getElementById('book-list');
-
         data.books.forEach(book => {
             const bookItem = document.createElement('div');
             bookItem.className = 'book';
-
             bookItem.innerHTML = `
                 <img src="${book.image}" alt="${book.title}">
                 <h3>${book.title}</h3>
                 <p>${book.author}</p>
                 <p><em>${book.category}</em></p>
-                <a href="${book.link}" target="_blank">
-                    <i class="fa-brands fa-amazon"></i> View on Amazon
-                </a>
+                <a href="${book.link}" target="_blank"><i class="fa-brands fa-amazon"></i> View on Amazon</a>
             `;
-
             bookList.appendChild(bookItem);
         });
     })
